42011c4584ac71cb33c1500f8459e928d02b6d86
dmamanipar <dmamanipar@unsa.edu.pe>
2026-04-13 12:52:41 -0500

    servicio

--- SysVentVehiculo/src/main/java/pe/edu/upeu/repository/ClienteRepository.java
+++ SysVentVehiculo/src/main/java/pe/edu/upeu/repository/ClienteRepository.java
@@ -1,4 +1,35 @@
 package pe.edu.upeu.repository;
 
+import pe.edu.upeu.model.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClienteRepository {
+    public static ClienteRepository instace=new ClienteRepository();
+
+    public static ClienteRepository getInstace(){
+        if(instace==null){
+            instace=new ClienteRepository();
+        }
+        return instace;
+    }
+    List<Cliente> clientes=new ArrayList<>();
+    //Create
+    public void save(Cliente cliente){
+        clientes.add(cliente);
+    }
+    //Report
+    public List<Cliente> finAll(){
+        return clientes;
+    }
+    //Update
+    public void  update(Cliente c, int index){
+        clientes.set(index, c);
+    }
+    //Delete
+    public void delete(int index){
+        clientes.remove(index);
+    }
+
 }
