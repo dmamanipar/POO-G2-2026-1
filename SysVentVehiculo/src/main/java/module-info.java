aa52522b266a000149d867fad2f2894db42445e9
dmamanipar <dmamanipar@unsa.edu.pe>
2026-04-15 16:06:15 -0500

    disenho interfaz

--- SysVentVehiculo/src/main/java/module-info.java
+++ SysVentVehiculo/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module pe.edu.upeu {
     requires javafx.fxml;
     requires javafx.graphics;
     requires static lombok;
+    requires org.kordamp.bootstrapfx.core;
 
     opens pe.edu.upeu.controller to javafx.fxml;
 
--- SysVentVehiculo/src/main/java/pe/edu/upeu/App.java
+++ SysVentVehiculo/src/main/java/pe/edu/upeu/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 public class App extends Application {
 
@@ -16,6 +17,7 @@ public class App extends Application {
         FXMLLoader loand=new FXMLLoader(getClass()
                 .getResource("/view/maingui.fxml"));
         Scene scene=new Scene(loand.load(), 400, 600);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.setScene(scene);
         stage.show();
     }
